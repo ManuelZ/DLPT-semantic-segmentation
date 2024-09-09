c3bb93ae25b945de1d3187c88451cecdcca48cb0
ManuelZ <115771+ManuelZ@users.noreply.github.com>
2024-09-09 14:19:37 -0500

    refactor

--- semantic_segmentation/model.py
+++ semantic_segmentation/model.py
@@ -17,3 +17,27 @@ def make_model(num_classes, device):
         param.requires_grad = False
 
     return model.to(device)
+
+
+def unfreeze_deeplabv3(model, layers=None):
+
+    if layers is None:
+        # Unfreeze whole backbone
+        for name, param in model.named_parameters():
+            param.requires_grad = True
+    else:
+        print("To be done")
+        pass
+
+    # Set the last backbone layers to be trainable
+    # for param in model.backbone.layer4.parameters():
+    #     param.requires_grad = True
+
+    # for param in model.backbone.layer3.parameters():
+    #     param.requires_grad = True
+
+    # for param in model.backbone.layer2.parameters():
+    #     param.requires_grad = True
+
+    # for param in model.backbone.layer1.parameters():
+    #     param.requires_grad = True
