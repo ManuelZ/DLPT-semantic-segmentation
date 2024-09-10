7e72b82eacb3bf11deb2803ee9635aa4eca09713
ManuelZ <115771+ManuelZ@users.noreply.github.com>
2024-09-10 11:37:45 -0500

    Fix

--- semantic_segmentation/model.py
+++ semantic_segmentation/model.py
@@ -24,14 +24,13 @@ def unfreeze_resnet101_backbone(model, layers: list[str] | None = None):
 
     resnet101_layers = ["layer1", "layer2", "layer3", "layer4"]
 
-    # Unfreeze only certain layers
-    if layers:
+    # Unfreeze whole backbone
+    if layers is None:
+        for param in model.parameters():
+            param.requires_grad = True
+    else:  # Unfreeze only certain specified layers
         for layer in layers:
             assert layer in resnet101_layers, f"Layer {layer} isn't part of Resnet101"
             for param in getattr(model.backbone, layer).parameters():
                 param.requires_grad = True
             print(f"Unfreezing parameters of layer {layer}")
-    else:  # Unfreeze whole backbone
-        for param in model.parameters():
-            param.requires_grad = True
-        print(f"Unfreezing all backbone layers")
