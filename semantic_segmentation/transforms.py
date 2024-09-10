e06ae9d237088c0934e2d23c459ff527bfb61eb4
ManuelZ <115771+ManuelZ@users.noreply.github.com>
2024-09-10 01:53:48 -0500

    Change probabilities of augmentations

--- semantic_segmentation/transforms.py
+++ semantic_segmentation/transforms.py
@@ -7,8 +7,7 @@ def get_transforms(mask_fill_value):
 
     train_transforms = A.Compose(
         [
-            A.HorizontalFlip(p=0.5),
-            A.VerticalFlip(p=0.5),
+            A.HorizontalFlip(p=0.1),
             A.Rotate(
                 limit=10,
                 border_mode=cv2.BORDER_CONSTANT,
@@ -31,7 +30,7 @@ def get_transforms(mask_fill_value):
                 mask_value=mask_fill_value,
                 border_mode=cv2.BORDER_CONSTANT,
                 interpolation=cv2.INTER_CUBIC,
-                p=0.75,
+                p=0.1,
             ),
             A.GridDistortion(
                 num_steps=10,
@@ -40,24 +39,24 @@ def get_transforms(mask_fill_value):
                 mask_value=mask_fill_value,
                 border_mode=cv2.BORDER_CONSTANT,
                 interpolation=cv2.INTER_CUBIC,
-                p=0.75,
+                p=0.1,
             ),
-            A.OpticalDistortion(distort_limit=0.05, shift_limit=0.05, p=0.75),
+            A.OpticalDistortion(distort_limit=0.05, shift_limit=0.05, p=0.1),
             A.ColorJitter(
                 brightness=(0.5, 1.5),
                 contrast=(0.5, 1.5),
                 saturation=(0.5, 1.5),
                 hue=0.01,  # must be in this interval [-0.5, 0.5].
-                p=0.75,
+                p=0.1,
             ),
-            A.PixelDropout(p=0.5),
+            A.PixelDropout(p=0.1),
             A.CoarseDropout(p=0.1),
-            A.ISONoise(p=0.5),
+            A.ISONoise(p=0.1),
             A.Blur(p=0.01),
             A.MedianBlur(p=0.01),
             A.ToGray(p=0.01),
             A.CLAHE(p=0.01),
-            A.RandomGamma(p=0.5),
+            A.RandomGamma(p=0.1),
             A.ImageCompression(quality_lower=75, p=0.01),
             A.Perspective(fit_output=True, pad_mode=cv2.BORDER_CONSTANT, p=0.1),
             A.Normalize(),
