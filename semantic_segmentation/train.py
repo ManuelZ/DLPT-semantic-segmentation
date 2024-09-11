7e6484d27b4624030f1631c44298cbe0eb8621cd
ManuelZ <115771+ManuelZ@users.noreply.github.com>
2024-09-11 01:39:42 -0500

    Rename variables, rewrite log messages, clean comments, rename test() to validate()

--- semantic_segmentation/train.py
+++ semantic_segmentation/train.py
@@ -120,8 +120,8 @@ def train(
     epoch_score = 0
 
     num_steps = len(dataloader.dataset) // batch_size
-    train_prog_bar = tqdm(dataloader, total=num_steps)
-    for batch_index, (x, y) in enumerate(train_prog_bar):
+    prog_bar = tqdm(dataloader, total=num_steps)
+    for batch_index, (x, y) in enumerate(prog_bar):
 
         x = x.to(device, dtype=torch.float32)
         y = y.squeeze()
@@ -162,18 +162,19 @@ def train(
 
             optimizer.zero_grad()  # TODO: test passing set_to_none=True
 
-        train_prog_bar.set_description(
-            desc=f"Training loss: {loss.item():.4f} | Mean Dice score: {float(train_score.mean()):.2f}"
+        prog_bar.set_description(
+            desc=f"Training loss: {loss.item():.4f} | score: {float(train_score.mean()):.2f}"
         )
 
     # Average train metrics during the epoch
-    avg_train_loss = epoch_loss / num_steps
-    avg_train_score = epoch_score / num_steps
+    avg_loss = epoch_loss / num_steps
+    avg_score = epoch_score / num_steps
 
-    return avg_train_loss, avg_train_score
+    return avg_loss, avg_score
 
 
-def test(model, loss_fun, scorer, dataloader, batch_size, device):
+def validate(model, loss_fun, scorer, dataloader, batch_size, device):
+    """ """
 
     epoch_loss = 0
     epoch_score = 0
@@ -183,34 +184,34 @@ def test(model, loss_fun, scorer, dataloader, batch_size, device):
     with torch.no_grad():
 
         num_steps = len(dataloader.dataset) // batch_size
-        valid_prog_bar = tqdm(dataloader, total=num_steps)
-        for x, y in valid_prog_bar:
+        prog_bar = tqdm(dataloader, total=num_steps)
+        for x, y in prog_bar:
             x = x.to(device, dtype=torch.float32)
             y = y.squeeze()
             y = y.to(device, dtype=torch.long)
 
-            # Validation loss
+            # Loss
             pred_logits = model(x)["out"]
             loss = loss_fun(pred_logits, y)
             epoch_loss += loss.item()
 
-            # Validation score
+            # Score
             pred_probs = pred_logits.softmax(dim=1)
             max_indices = pred_probs.argmax(dim=1)
-            valid_score = scorer(max_indices, y)
-            epoch_score += float(valid_score.mean())
+            score = scorer(max_indices, y)
+            epoch_score += float(score.mean())
 
             # Per-class validation score
-            per_class_score.append(valid_score.reshape(1, -1))
+            per_class_score.append(score.reshape(1, -1))
 
-            valid_prog_bar.set_description(
-                desc=f"Validation loss: {loss.item():.4f} | Mean Dice score: {float(valid_score.mean()):.2f}"
+            prog_bar.set_description(
+                desc=f"Validation loss: {loss.item():.4f} | score: {float(score.mean()):.2f}"
             )
 
         # Average validation metrics during the epoch
-        avg_valid_loss = epoch_loss / num_steps
-        avg_valid_score = epoch_score / num_steps
+        avg_loss = epoch_loss / num_steps
+        avg_score = epoch_score / num_steps
         avg_per_class_score = (
             np.concatenate(per_class_score, axis=0).sum(axis=0) / num_steps
         )
-    return avg_valid_loss, avg_valid_score, avg_per_class_score
+    return avg_loss, avg_score, avg_per_class_score
